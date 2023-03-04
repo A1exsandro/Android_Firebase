f2119b045dea9da757cec8e9a30a3e2b5d02b8a9
A1exsandro <alexsandro.acp@gmail.com>
2023-03-04 18:59:51 -0400

    import firebase

--- app/src/main/java/com/nst/androidfirebase/MainActivity.kt
+++ app/src/main/java/com/nst/androidfirebase/MainActivity.kt
@@ -2,8 +2,11 @@ package com.nst.androidfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    var storage = Firebase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
