b71eba7518d575ecf7a2b43ccfd61cbd8aa9fd22
A1exsandro <alexsandro.acp@gmail.com>
2023-03-18 19:11:16 -0400

    refactor layout sign up

--- app/src/main/java/com/nst/androidfirebase/ui/auth/SignUpFragment.kt
+++ app/src/main/java/com/nst/androidfirebase/ui/auth/SignUpFragment.kt
@@ -6,15 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nst.androidfirebase.R
+import com.nst.androidfirebase.databinding.FragmentLoginBinding
+import com.nst.androidfirebase.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
+
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
