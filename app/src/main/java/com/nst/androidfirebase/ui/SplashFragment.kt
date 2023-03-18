66e239506733556ecdb7cc5a1eeb6500ef328457
A1exsandro <alexsandro.acp@gmail.com>
2023-03-18 18:24:42 -0400

    first navigation

--- app/src/main/java/com/nst/androidfirebase/ui/SplashFragment.kt
+++ app/src/main/java/com/nst/androidfirebase/ui/SplashFragment.kt
@@ -1,19 +1,41 @@
 package com.nst.androidfirebase.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.nst.androidfirebase.R
+import com.nst.androidfirebase.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, 3000)
+    }
+
+    private fun checkAuth() {
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment2)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
