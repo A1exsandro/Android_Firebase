4e2ba04e4deb70a7247951a8abfe81e569af92ee
A1exsandro <alexsandro.acp@gmail.com>
2023-03-18 17:47:33 -0400

    add graph navigation

--- app/src/main/java/com/nst/androidfirebase/MainActivity.kt
+++ app/src/main/java/com/nst/androidfirebase/MainActivity.kt
@@ -2,12 +2,19 @@ package com.nst.androidfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.google.firebase.ktx.Firebase
+import com.nst.androidfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
     }
 }
