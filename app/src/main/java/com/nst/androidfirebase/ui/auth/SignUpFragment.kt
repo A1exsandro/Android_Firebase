054e12a8c90e565ee1611e9bc085bceb67573ab5
A1exsandro <alexsandro.acp@gmail.com>
2023-03-18 19:23:18 -0400

    add home fragment and navigation

--- app/src/main/java/com/nst/androidfirebase/ui/auth/SignUpFragment.kt
+++ app/src/main/java/com/nst/androidfirebase/ui/auth/SignUpFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
 import com.nst.androidfirebase.R
 import com.nst.androidfirebase.databinding.FragmentLoginBinding
 import com.nst.androidfirebase.databinding.FragmentSignUpBinding
@@ -20,6 +22,27 @@ class SignUpFragment : Fragment() {
     ): View? {
         _binding = FragmentSignUpBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    private fun checkData() {
+        val email = binding.editEmail.toString().trim()
+        val password = binding.editPassword.toString().trim()
+
+        if (email.isNotBlank()) {
+            if (password.isNotBlank()) {
+                binding.progressBar.isVisible = true
+
+                registerUser(email, password)
+            } else {
+                Toast.makeText(requireContext(), "Informe seu password", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Toast.makeText(requireContext(), "Informe seu e-mail", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun registerUser(email: String, password: String) {
+
     }
 
     override fun onDestroy() {
