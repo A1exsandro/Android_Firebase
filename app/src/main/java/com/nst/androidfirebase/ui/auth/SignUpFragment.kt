17cf1493d20c29099f61a6e43ec4c770c70ebfe3
A1exsandro <alexsandro.acp@gmail.com>
2023-03-22 14:33:08 -0400

    sign up fragment

--- app/src/main/java/com/nst/androidfirebase/ui/auth/SignUpFragment.kt
+++ app/src/main/java/com/nst/androidfirebase/ui/auth/SignUpFragment.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.nst.androidfirebase.R
 import com.nst.androidfirebase.databinding.FragmentLoginBinding
 import com.nst.androidfirebase.databinding.FragmentSignUpBinding
@@ -16,12 +20,25 @@ class SignUpFragment : Fragment() {
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSignUpBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        auth = Firebase.auth
+        initClicks()
+    }
+
+    private fun initClicks() {
+        binding.btnSignUp.setOnClickListener { checkData() }
     }
 
     private fun checkData() {
@@ -42,7 +59,14 @@ class SignUpFragment : Fragment() {
     }
 
     private fun registerUser(email: String, password: String) {
-
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    findNavController().navigate(R.id.action_signUpFragment_to_homeFragment)
+                } else {
+                    binding.progressBar.isVisible = false
+                }
+            }
     }
 
     override fun onDestroy() {
