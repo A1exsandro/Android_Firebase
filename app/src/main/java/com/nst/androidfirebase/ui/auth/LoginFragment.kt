35b417a280aee240008d07a461a24350383ba11c
A1exsandro <alexsandro.acp@gmail.com>
2023-03-24 13:36:52 -0400

    refactor Login fragment

--- app/src/main/java/com/nst/androidfirebase/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/nst/androidfirebase/ui/auth/LoginFragment.kt
@@ -68,7 +68,7 @@ class LoginFragment : Fragment() {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(requireActivity()) { task ->
                 if (task.isSuccessful) {
-                    findNavController().navigate(R.id.action_loginFragment2_to_homeFragment)
+                    findNavController().navigate(R.id.action_global_homeFragment)
                 } else {
                     binding.progressBar.isVisible = false
                 }
