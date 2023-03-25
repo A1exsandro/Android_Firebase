97da1b3ee53b2eb211804ac129444f1631498689
A1exsandro <alexsandro.acp@gmail.com>
2023-03-25 11:25:49 -0400

    add firebase helper class

---
+++ app/src/main/java/com/nst/androidfirebase/helper/FirebaseHelper.kt
@@ -0,0 +1,25 @@
+package com.nst.androidfirebase.helper
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+
+class FirebaseHelper {
+
+    companion object {
+        fun getDatabase() = FirebaseDatabase.getInstance().reference
+        private fun getAuth() = FirebaseAuth.getInstance()
+        fun getIdUser() = getAuth().uid
+        fun isAuthenticated() = getAuth().currentUser != null
+
+        fun validError(error: String): Int {
+            return when {
+                error.contains("") -> {
+                    1
+                }
+                else -> {
+                    0
+                }
+            }
+        }
+    }
+}
