4fd646f7d322ba07b6bae8316aecca9477d703c0
A1exsandro <alexsandro.acp@gmail.com>
2023-03-27 14:07:35 -0400

    first test realtime database

--- app/src/main/java/com/nst/androidfirebase/ui/FormTaskFragment.kt
+++ app/src/main/java/com/nst/androidfirebase/ui/FormTaskFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.findNavController
 import com.nst.androidfirebase.R
 import com.nst.androidfirebase.databinding.FragmentFormTaskBinding
+import com.nst.androidfirebase.helper.FirebaseHelper
+import com.nst.androidfirebase.model.Task
 
 class FormTaskFragment : Fragment() {
 
     private var _binding: FragmentFormTaskBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var task: Task
+    private var newTask: Boolean = true
+    private var statusTask: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +28,73 @@ class FormTaskFragment : Fragment() {
     ): View {
         _binding = FragmentFormTaskBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.btnSave.setOnClickListener { validateData() }
+
+        binding.radioGroup.setOnCheckedChangeListener { _, id ->
+            statusTask = when (id) {
+                R.id.rbTodo -> 0
+                R.id.rbDoing -> 1
+                else -> 2
+            }
+        }
+    }
+
+    private fun validateData() {
+        binding.btnSave.setOnClickListener {
+            val description = binding.editText.text.toString().trim()
+
+            if (description.isNotEmpty()) {
+                binding.progressBar.isVisible = true
+
+                if (newTask) task = Task()
+                task.description = description
+                task.status = statusTask
+
+                saveTask()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Informe uma descrição para a tarefa.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    private fun saveTask() {
+        FirebaseHelper
+            .getDatabase()
+            .child("task")
+            .child(FirebaseHelper.getIdUser() ?: "")
+            .child(task.id)
+            .setValue(task)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    if (newTask) {
+                        // New Task
+                        Toast.makeText(requireContext(), "Tarefa salva com sucesso", Toast.LENGTH_SHORT).show()
+                        findNavController().popBackStack()
+                    } else {
+                        // Editing Task
+                        binding.progressBar.isVisible = false
+                        Toast.makeText(requireContext(), "Tarefa Atualizada com sucesso", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(requireContext(), "Erro ao salva tarefa", Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener {
+                binding.progressBar.isVisible = false
+                Toast.makeText(requireContext(), "Erro ao salva tarefa", Toast.LENGTH_SHORT).show()
+            }
     }
 
     override fun onDestroy() {
