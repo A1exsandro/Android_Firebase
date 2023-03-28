52e335dc6bd8b20040c96c5e357a1b9e6eda3a0f
A1exsandro <alexsandro.acp@gmail.com>
2023-03-28 10:36:27 -0400

    refactor BaseFragment

--- app/src/main/java/com/nst/androidfirebase/helper/BaseFragment.kt
+++ app/src/main/java/com/nst/androidfirebase/helper/BaseFragment.kt
@@ -1,5 +1,6 @@
 package com.nst.androidfirebase.helper
 
+import android.content.Context
 import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 
@@ -8,8 +9,8 @@ class BaseFragment : Fragment() {
     fun hideKeyboard() {
         val view = activity?.currentFocus
         if (view != null) {
-            val imm= activity?.getSystemService(context.INPUT_METHOD_SERVICE) AS InputMethodManager
-            imm.hideSoftInputFromWindow(view.windowToken, 0)
+            val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager?
+            imm?.hideSoftInputFromWindow(view?.windowToken, 0)
         }
     }
 }
